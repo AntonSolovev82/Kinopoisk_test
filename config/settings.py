17a7fc0fb841a66ee276ee81587c4d6f5043920f
AntonSolovev82 <antonsolovev1982@gmail.com>
2025-03-19 02:13:23 +0300

    Готовы api тесты

---
+++ config/settings.py
@@ -0,0 +1,5 @@
+BASE_URL_API = "https://api.kinopoisk.dev/v1.4"
+API_TOKEN = {"X-API-KEY": "CRC3A21-NBJ4CMN-NKNDX3F-G271NN9"}
+BASE_URL="https://www.kinopoisk.ru/?utm_referrer=www.kinopoisk.ru"
+GENRE = "криминал"
+YEAR = "2024"
