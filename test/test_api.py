dc89730d7e602a7f831f08c67369d3c1b8a5ec4f
AntonSolovev82 <antonsolovev1982@gmail.com>
2025-03-26 23:54:43 +0300

    Последняя версия всех тестов и изменный readme

--- test/test_api.py
+++ test/test_api.py
@@ -1,82 +1,64 @@
 import allure
 import requests
-from config.settings import BASE_URL_API, API_TOKEN, YEAR, GENRE, ACTOR_EN
+from config.settings import BASE_URL_API, API_TOKEN, YEAR, GENRE, ACTOR_EN, MOVIE_NAME
 
 @allure.feature("Поиск фильмов")
 @allure.story("Поиск фильма по названию")
-@allure.title("Тест: Поиск фильма по названию 'Дюна'")
+@allure.title(f"Тест: Поиск фильма по названию {MOVIE_NAME}")
 @allure.description("Проверяем, что API возвращает фильм с названием 'Дюна' и статус 200.")
 def test_search_movie_by_title():
-    title = "Дюна"
-    response = requests.get(f"{BASE_URL_API}/movie/search?query={title}", headers=API_TOKEN)
-
+    response = requests.get(f"{BASE_URL_API}/movie/search?query={MOVIE_NAME}", headers=API_TOKEN)
     with allure.step("Проверка статуса ответа"):
         assert response.status_code == 200, f"Ожидался статус 200, но получен {response.status_code}"
-
     response_title = response.json()["docs"][0]["name"]
     with allure.step("Проверка названия фильма в ответе"):
-        assert response_title == title, f"Ожидалось название '{title}', но получено '{response_title}'"
-
+        assert response_title == MOVIE_NAME, f"Ожидалось название '{MOVIE_NAME}', но получено '{response_title}'"
 @allure.story("Поиск фильма по жанру в определенный год")
 @allure.title("Тест: Поиск фильмов по жанру и году")
 @allure.description("Проверяем, что API возвращает фильмы определенного жанра за указанный год.")
 def test_search_movie_by_genre():
     genre_to_search = GENRE
     response = requests.get(f"{BASE_URL_API}/movie?year={YEAR}&genres.name={genre_to_search}", headers=API_TOKEN)
-
     with allure.step("Проверка статуса ответа"):
         assert response.status_code == 200, f"Ошибка запроса: {response.status_code}"
-
     result_search = response.json()
     movies_list = result_search.get("docs", [])
-
     with allure.step("Проверка наличия фильмов в ответе"):
         assert movies_list, "Нет фильмов для заданных параметров"
-
     found_genre = False
     for movie in movies_list:
         genres = movie.get("genres", [])
         if any(genre["name"] == genre_to_search for genre in genres):
             found_genre = True
             break
-
     with allure.step("Проверка наличия жанра в результатах"):
         assert found_genre, f"Жанр '{genre_to_search}' не найден в результатах"
-
 @allure.story("Поиск актера")
 @allure.title(f"Тест: Поиск актера '{ACTOR_EN}'")
 @allure.description(f"Проверяем, что API возвращает актера с именем '{ACTOR_EN}'.")
 def test_search_actor():
     response = requests.get(f"{BASE_URL_API}/person/search?query={ACTOR_EN}", headers=API_TOKEN)
-
     with allure.step("Проверка статуса ответа"):
         assert response.status_code == 200, f"Ожидался статус 200, но получен {response.status_code}"
-
     response_name = response.json()["docs"][0]["enName"]
     with allure.step("Проверка имени актера в ответе"):
         assert response_name == ACTOR_EN, f"Ожидалось имя '{ACTOR_EN}', но получено '{response_name}'"
-
 @allure.story("Поиск с пустым запросом")
 @allure.title("Тест: Поиск с пустым запросом")
 @allure.description("Проверяем, что API возвращает статус 200 при пустом запросе на поиск фильмов.")
 def test_search_with_empty_query():
     response = requests.get(f"{BASE_URL_API}/movie/search?", headers=API_TOKEN)
-
     with allure.step("Проверка статуса ответа"):
         assert response.status_code == 200, f"Ожидался статус 200, но получен {response.status_code}"
-
-
 @allure.story("Поиск с другим методом (POST)")
 @allure.title("Негативный тест: Поиск фильма с некорректными параметрами")
 @allure.description("Проверяем, что API возвращает статус 404 при поиске фильма с использованием метода POST без необходимых параметров.")
 def test_search_movie_post():
     # Выполняем POST-запрос
     response = requests.post(f"{BASE_URL_API}/movie/search?", headers=API_TOKEN)
-
     # Проверяем статус-код ответа
     with allure.step("Проверка статуса ответа"):
         assert response.status_code == 404, f"Ожидался статус 404, но получен {response.status_code}"
-
     # Дополнительно можно добавить информацию о теле ответа
     with allure.step("Проверка тела ответа"):
         allure.attach(response.text, "Ответ от API", allure.attachment_type.TEXT)
--- test/test_ui.py
+++ test/test_ui.py
@@ -9,19 +9,8 @@ from selenium.webdriver.support import expected_conditions as EC
 from config.settings import BASE_URL, MOVIE_NAME, ACTOR, NO_MOVIE_NAME
 from data.test_data import USER_CREDENTIALS
 from selenium.common.exceptions import NoSuchElementException
-"""
-             Возможно stealth может помочь в обходе капчи....
-from selenium_stealth import stealth
-"""
-"""
-           Для отладки  кода используем sleep , но не используем метод в тестировании   
-import time
-"""
-
-
 
 class Test_UI:
-
     @pytest.fixture(scope="function", autouse=True)
     def setup(self):
         with allure.step ("Заходим на сайт Кинопоиск"):
@@ -33,16 +22,13 @@ class Test_UI:
                 self.driver.find_element(By.CSS_SELECTOR, ".CheckboxCaptcha-Button").click()
             except NoSuchElementException:
                 pass
-
         with allure.step("Если появляется реклама, сбрасываем, иначе продолжаем следующие шаги."):
             try:
                 self.driver.find_element(By.CSS_SELECTOR, ".styles_root__EjoL7").click()
             except NoSuchElementException:
                 pass
-
         yield
         self.driver.quit()
-
     @allure.feature("Авторизация на сайте.")
     @allure.title("Тест авторизации пользователя.")
     @allure.description("Проверка успешной авторизации пользователя. Авторизуемся на сайте, используя входные данные.")
@@ -53,13 +39,10 @@ class Test_UI:
             self.driver.find_element(By.CSS_SELECTOR, ".styles_loginButton__LWZQp").click()
         with allure.step("Переключаемся на форму ввода логина почты"):
             try:
-                # Ждем, пока элемент будет доступен и видим
                 more_button = WebDriverWait(self.driver, 10).until(
                     EC.element_to_be_clickable((By.LINK_TEXT, "Ещё"))
                 )
                 more_button.click()
-
-                # Ждем появления кнопки в выпадающем списке и кликаем по ней
                 registration_buttons = WebDriverWait(self.driver, 10).until(
                     EC.presence_of_all_elements_located((By.CSS_SELECTOR, ".RegistrationButtonPopup-itemButton"))
                 )
@@ -67,26 +50,24 @@ class Test_UI:
                     registration_buttons[2].click()  # Индекс 2 соответствует третьей кнопке
                 else:
                     print("Третья кнопка не найдена.")
-
             except TimeoutException:
                 assert False, "Не удалось найти или нажать кнопку 'Ещё' или кнопку регистрации"
-        with allure.step("Заполняем поле логина."):
-            self.driver.find_element(By.CSS_SELECTOR, "#passp-field-login").send_keys(USER_CREDENTIALS['username'])
-            self.driver.find_element(By.CSS_SELECTOR, ".Button2-Text").click()
+        with (allure.step("Заполняем поле логина.")):
+            login_input=self.driver.find_element(By.CSS_SELECTOR, "#passp-field-login")
+            login_input.send_keys(USER_CREDENTIALS['username'])
+            login_input.send_keys(Keys.RETURN)
         with allure.step("Заполняем поле пароля."):
             password_input = self.driver.find_element(By.NAME, "passwd")
             password_input.send_keys(USER_CREDENTIALS['password'])
             password_input.send_keys(Keys.RETURN)
-
-        with allure.step("Проверяем успешную авторизацию."):
+        with allure.step("Проверяем неуспешную авторизацию."):
             try:
                 WebDriverWait(self.driver, 10).until(
-                    EC.presence_of_element_located((By.CSS_SELECTOR, ".styles_userName__XYZ"))  # Заменить на актуальный селектор
+                    EC.presence_of_element_located((By.ID, "field:input-passwd:hint"))
                 )
-                assert True  # Успешная авторизация
+                assert self.driver.find_element(By.ID, "field:input-passwd:hint").text == "Неверный пароль"# Успешная авторизация
             except TimeoutException:
                 assert False, "Авторизация не удалась"
-
     @allure.title("Тест восстановления пароля")
     @allure.description("Проверка процесса восстановления пароля")
     @allure.id(2)
@@ -96,13 +77,10 @@ class Test_UI:
             self.driver.find_element(By.CSS_SELECTOR, ".styles_loginButton__LWZQp").click()
         with allure.step("Переключаемся на форму ввода логина почты"):
             try:
-                # Ждем, пока элемент будет доступен и видим
                 more_button = WebDriverWait(self.driver, 10).until(
                     EC.element_to_be_clickable((By.LINK_TEXT, "Ещё"))
                 )
                 more_button.click()
-
-                # Ждем появления кнопки в выпадающем списке и кликаем по ней
                 registration_buttons = WebDriverWait(self.driver, 10).until(
                     EC.presence_of_all_elements_located((By.CSS_SELECTOR, ".RegistrationButtonPopup-itemButton"))
                 )
@@ -110,7 +88,6 @@ class Test_UI:
                     registration_buttons[2].click()  # Индекс 2 соответствует третьей кнопке
                 else:
                     print("Третья кнопка не найдена.")
-
             except TimeoutException:
                 assert False, "Не удалось найти или нажать кнопку 'Ещё' или кнопку регистрации"
         with allure.step("Заполняем поле логина."):
@@ -118,10 +95,12 @@ class Test_UI:
             login_input.send_keys(USER_CREDENTIALS['username'])
             login_input.send_keys(Keys.RETURN)
         with allure.step("Нажимаем 'Не помню пароль'"):
-            self.driver.find_element(By.CSS_SELECTOR, "a.Link.Link_pseudo.Link_view_default.Link_weight_medium").click()
-
-        assert "Check your email" in self.driver.page_source  # Проверка сообщения об успешной отправке
-
+            #self.driver.find_element(By.CSS_SELECTOR,".Link Link_pseudo Link_view_default Link_weight_medium").click()
+            button = WebDriverWait(self.driver, 10).until(
+                EC.element_to_be_clickable((By.CSS_SELECTOR, "a.Link.Link_pseudo.Link_view_default.Link_weight_medium"))
+            )
+            button.click()
+        assert self.driver.find_element(By.CSS_SELECTOR, ".Field-label").text == "Введите символы с картинки"
     @allure.title("Тест поиска фильма по названию")
     @allure.description("Проверка поиска фильма по его названию")
     @allure.id(3)
@@ -130,14 +109,11 @@ class Test_UI:
         search_bar = self.driver.find_element(By.CSS_SELECTOR, "input[placeholder='Фильмы, сериалы, персоны']")
         search_bar.send_keys(f"{MOVIE_NAME}")
         search_bar.send_keys(Keys.ENTER)
-
         movie_link = WebDriverWait(self.driver, 10).until(
             EC.element_to_be_clickable((By.LINK_TEXT, f"{MOVIE_NAME}"))
         )
         movie_link.click()
-
         assert f"{MOVIE_NAME}" in self.driver.title, f"Не удалось открыть страницу фильма '{MOVIE_NAME}'"
-
     @allure.title("Тест поиска по актеру")
     @allure.description("Проверка поиска фильмов по имени актера")
     @allure.id(4)
@@ -146,14 +122,11 @@ class Test_UI:
         search_bar = self.driver.find_element(By.CSS_SELECTOR, "input[placeholder='Фильмы, сериалы, персоны']")
         search_bar.send_keys(f"{ACTOR}")
         search_bar.send_keys(Keys.ENTER)
-
         actor_link = WebDriverWait(self.driver, 10).until(
             EC.element_to_be_clickable((By.LINK_TEXT, f"{ACTOR}"))
         )
         actor_link.click()
-
         assert f"{ACTOR}" in self.driver.title, f"Не удалось открыть страницу актера  '{ACTOR}'"
-
     @allure.title("Тест поиска по несуществующему названию")
     @allure.description("Проверка поведения системы при поиске по несуществующему названию фильма")
     @allure.id(5)
@@ -164,7 +137,7 @@ class Test_UI:
         search_bar.send_keys(Keys.ENTER)
         with (allure.step("Поиск в ответе нужного результата")):
             try :
-                no_movie_name =self.driver.find_elements("//*[contains(., 'К сожалению, по вашему запросу ничего не найдено...')]") ###?????
-                assert len(no_movie_name)>0
+                no_movie_name = self.driver.find_elements(By.CSS_SELECTOR, ".textorangebig")
+                assert no_movie_name[0].text == "К сожалению, по вашему запросу ничего не найдено..."
             except TimeoutException:
                 assert False,"Ошибка поиска "
